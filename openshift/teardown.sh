4b5e6b14d6a8bf44fb0391ffdecb754e8e717b3c
hinriksnaer <hgudmund@redhat.com>
2026-05-10 00:55:32 -0400

    Remove Nix store PVC mount -- use ephemeral /nix backed by pre-baked image

--- openshift/teardown.sh
+++ openshift/teardown.sh
@@ -9,5 +9,4 @@ USERNAME="${1:?Usage: $0 <username>}"
 echo "Deleting deployment for ${USERNAME}..."
 oc delete deployment "${USERNAME}-dev" -n "${USERNAME}" --ignore-not-found
 
-echo "Deployment deleted. PVCs preserved."
-echo "To also delete PVCs: oc delete pvc nix-store-${USERNAME} -n ${USERNAME}"
+echo "Deployment deleted."
